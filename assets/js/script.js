b9e0b650f92edd7c3636e6e5590fa29d92e8dc78
João <joaovictorpfr@gmail.com>
2021-10-11 08:55:00 -0300

    Average Notes V2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,15 @@
-let calcBtn = document.getElementById("media-calc");
-calcBtn;addEventListener("click", function calcMedia(){
-
+let calcBtn = document.getElementById("submit");
+calcBtn;
+addEventListener("click", function calcMedia() {
   let lista = [];
-
-  for(let i = 0; i < 3; i ++) {
-    lista[i] = parseFloat(document.getElementById("nota" + (i + 1)).value);
+  for (let i = 0; i < 3; i++) {
+    lista[i] = parseFloat(document.getElementById("note" + (i + 1)).value);
   }
-
   console.log(lista);
-
-  let resultNotes = document.getElementById("media");
+  let resultNotes = document.getElementById("result");
   let result = (lista[0] + lista[1] + lista[2]) / 3;
-  resultNotes.innerHTML = ("Sua média final foi:" + Math.round(result));
-
-  if (result <= 0){
-
-    resultNotes.innerHTML = "Sua Nota é inválida!"
+  resultNotes.innerHTML = "Final note: " + Math.round(result);
+  if (result <= 0) {
+    resultNotes.innerHTML = "Sua Nota é inválida!";
   }
 });
